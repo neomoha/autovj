61014d99462b46ba32d196e197cf263b50e6325c
Mohamed Sordo <mohamed.sordo@gmail.com>
2014-08-16 22:46:56 +0200

    added license

--- config.py
+++ config.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of AutoVJ.
+#
+# AutoVJ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AutoVJ is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with AutoVJ.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Mohamed Sordo (@neomoha)
+# Email: mohamed ^dot^ sordo ^at^ gmail ^dot^ com
+# Website: http://msordo.weebly.com
+
 
 LASTFM_API_KEY = u"YOUR_LASTM_API_KEY"
 LASTFM_API_SECRET = u"YOUR_LASTFM_API_SECRET"
